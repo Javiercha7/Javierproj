407ae6a7e9681a5bb05bc46d4fb070d1c5f25188
Javiercha7 <yerald.chaustre7@gmail.com>
2023-11-06 18:14:38 -0500

    updated code

--- Python/main.py
+++ Python/main.py
@@ -1,40 +1,52 @@
 Option1 = "yes"
 Option2 = "no"
+count = 0
 total = 0
 total2 = 0
-q1 = "Would You rather eat a Hawaian pizza or a Meatza? "
+Questions = ["Would You rather eat a Hawaian pizza or a Meatza? ", 
+              "Would You rather Go Chernobyl or North  Korea","Nothing"]
 # Question used for the user
-q2 = "Would You rather Go "
-#
+
+
+# q1 = "Would You rather eat a Hawaian pizza or a Meatza? "
+# # Question used for the user
+
+# q2 = "Would You rather Go Chernobyl or North  Korea"
+# # Question used for the user
+
+
 
 def userYes():
   global total 
   total = total + 1
-  print(f"Really me too!! Is the best pizza out there and is not debatable {total}")  
+  if total == 1 :
+    print(f"Really me too!! Is the best pizza out there and is not debatable \nThis many people support this option {total}")  
+  else :
+       print(f"You may gain superpowers like Homelander or just die but hey better than being in North Korea \nThis many people support this option {total}")
   # Add a total variable and displayis how many people aggre with your decision.
+
+
 
 def userNo():
   global total2
-  total2 = total + 1
-  print(f"Ohh nicee choice, could be better though {total2}")
-  # Add a total variable and displayis how many people aggre with your decision.
+  total2 = total2 + 1
+  if  total2 == 1 :
+    print(f"Ohh nicee choice, could be better though \nThis many people support this option {total2}")
+  else :
+       print(f"Say bye bye to your freedom! say hello to your forced marriage and getting exploit labor wise. \nThis many people support this option {total2}")
+ 
+  # Add a total variable and display is how many people aggre with your decision.
 
 
-responce1 = input(q1)
-# gets the data from the user in the program
+while count < 2 :
+    print(f'Select either yes(1) or no(2)')
+    responce1 = int(input(Questions[0]))
+    if responce1 == 1:
+        userYes()
+    else :
+        userNo()
+    
+    del Questions[0]
+count+= 1
+# gets the data from the user in the program  
 
-if responce1[0].lower() == "h":
-   userYes()
-# if the user answers wth a word that has a lowercase y in the beginning, it would print the total and a funny message
-  
-elif responce1[0].lower() == "m":
-  userNo()
-# if the user answers wth a word that has a lowercase n in the beginning, it would print the total and a funny message
-  
-
-
-# if user == "Yes":
-#  userYes()
-# elif user == "No":
-#   userNo()
-  
